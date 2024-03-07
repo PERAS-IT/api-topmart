91f48cb4872e3c46667f7e4c29509e24e03bf670
PERAS-IT <general.of.felasia@gmail.com>
2024-03-07 15:28:59 +0700

    add authenticate landing and fix bug router

--- src/router/landing.js
+++ src/router/landing.js
@@ -1,14 +1,34 @@
 const express = require("express");
 
+const authenticate = require("../middlewares/authenticate");
+const { checkPermission } = require("../middlewares/checkPermission");
+const { Role } = require("@prisma/client");
+
 const c = require("../controller");
 const { uploadMiddlewareLanding } = require("../middlewares/upload");
 
 const landingRoute = express.Router();
 
 //=======================================LANDING PAGE=====
-landingRoute.get("/", c.landing);
-landingRoute.post("/upload", uploadMiddlewareLanding, c.landing.uploadLanding);
-landingRoute.post("/delete/:landingId", c.landing.deleteLanding);
-landingRoute.post("/delete/multi", c.landing.deleteMultiLanding);
+landingRoute.get("/", c.landing.getLanding);
+landingRoute.post(
+  "/upload",
+  authenticate,
+  checkPermission(Role.ADMIN, Role.SUPERADMIN),
+  uploadMiddlewareLanding,
+  c.landing.uploadLanding
+);
+landingRoute.post(
+  "/delete/:landingId",
+  authenticate,
+  checkPermission(Role.ADMIN, Role.SUPERADMIN),
+  c.landing.deleteLanding
+);
+landingRoute.post(
+  "/delete/multi",
+  authenticate,
+  checkPermission(Role.ADMIN, Role.SUPERADMIN),
+  c.landing.deleteMultiLanding
+);
 
 module.exports = landingRoute;
