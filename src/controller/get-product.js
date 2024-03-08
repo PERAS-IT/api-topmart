3d509fb5f613670e3e6c023cf313992d8b357231
PERAS-IT <general.of.felasia@gmail.com>
2024-03-08 15:22:56 +0700

    edit prisma get product to Raw quary

--- src/controller/get-product.js
+++ src/controller/get-product.js
@@ -4,7 +4,7 @@ const repo = require("../repository");
 module.exports.getProductBySeriesId = async (req, res, next) => {
   try {
     const id = +req.params.seriesId;
-    const resultBySeriesId = await repo.getProduct.getProductBySeriesId(id);
+    const resultBySeriesId = await repo.getProduct.getProductBySeriesIdRaw(id);
     res.status(200).json({ resultBySeriesId });
   } catch (err) {
     console.log(err);
@@ -16,7 +16,7 @@ module.exports.getProductByGroup = async (req, res, next) => {
   try {
     const groupName = req.params.groupName;
     console.log(groupName);
-    const resultByGroup = await repo.getProduct.getProductByGroupName(
+    const resultByGroup = await repo.getProduct.getProductByGroupNameRaw(
       groupName
     );
     res.status(200).json({ resultByGroup });
@@ -29,9 +29,8 @@ module.exports.getProductByGroup = async (req, res, next) => {
 module.exports.getProductByCategoriesId = async (req, res, next) => {
   try {
     const categoriesId = +req.params.categoriesId;
-    const resultByCategories = await repo.getProduct.getProductByCategoriesId(
-      categoriesId
-    );
+    const resultByCategories =
+      await repo.getProduct.getProductByCategoriesIdRaw(categoriesId);
     res.status(200).json({ resultByCategories });
   } catch (err) {
     console.log(err);
